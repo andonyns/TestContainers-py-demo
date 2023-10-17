2bb48aff010473d6e5438c3241c80ca31bc16f66
Andony Nuñez <andony91@gmail.com>
2023-10-16 21:22:05 -0600

    Docker compose example

--- test_web.py
+++ test_web.py
@@ -1,7 +1,18 @@
 import re
 from playwright.sync_api import Page, expect
+from testcontainers.compose import DockerCompose
 
 def test_has_title(page: Page):
+
+
+    compose = DockerCompose("./", compose_file_name="docker-compose.yml",
+                        pull=True)
+    with compose:
+        stdout, stderr = compose.get_logs()
+
+    compose.start()
+    compose.wait_for("http://localhost:5000")
+
     page.goto("http://localhost:5000")
 
     # Expect a title "to contain" a substring.
